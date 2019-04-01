d3579885b5342594e1f067e9788b84ef097358bf
lin <1457815431@qq.com>
2019-04-02 00:34:24 +0800

    http 2

--- lib/http/http_demo.dart
+++ lib/http/http_demo.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
@@ -24,18 +26,94 @@ class _HoemDemoState extends State<HoemDemo> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    fetchData();
+    // fetchData().then((value) => print(value));
+    // final post = {
+    //   'title': 'hello',
+    //   'description': 'nice to me you',
+    // };
+    // print(post['title']);
+    // print(post['description']);
+    // print(post);
+
+    // final postJson = json.encode(post);
+    // print(postJson);
+
+    // final postJsonConverted = json.decode(postJson);
+    // print(postJsonConverted['title']);
+    // print(postJsonConverted['description']);
+    // print(postJsonConverted is Map);
+
+    // final postModel = Post.fromJson(postJsonConverted);
+    // print('title : ${postModel.title},description :${postModel.description}');
+
+    // print(postModel);
+    // print('${json.encode(postModel)}');
   }
 
-  fetchData() async {
-    final respone =
+  Future<List<Post>> fetchData() async {
+    final response =
         await http.get('https://resources.ninghao.net/demo/posts.json');
-    print('statusCode:${respone.statusCode}');
-    print('contents:${respone.body}');
+    // print(respone);
+    // print('statusCode:${respone.statusCode}');
+    // print('contents:${response.body}');
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      List<Post> posts =
+          responseBody['posts'].map<Post>((item) => Post.fromJson(item)).toList();
+      return posts;
+    } else {
+      throw Exception('Faile to fetchData.');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder(
+      future: fetchData(),
+      builder: (BuildContext context,AsyncSnapshot snapshot){
+        if(snapshot.connectionState==ConnectionState.waiting){
+          return Center(
+            child: Text('Loading...'),
+          );
+        }
+
+        return ListView(
+          children: snapshot.data.map<Widget>((item){
+            return ListTile(
+              title: Text(item.title),
+              subtitle: Text(item.author),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(item.imageUrl),
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
   }
+}
+
+class Post {
+  final int id;
+  final String title;
+  final String author;
+  final String description;
+  final String imageUrl;
+
+  Post(this.id, this.title,this.author, this.description, this.imageUrl);
+
+  Post.fromJson(Map json)
+      : id = json['id'],
+        title = json['title'],
+        author = json['author'],
+        description = json['description'],
+        imageUrl = json['imageUrl'];
+
+  Map toJson() => {
+        'id': id,
+        'title': title,
+        'author': author,
+        'description': description,
+        'imageUrl': imageUrl,
+      };
 }
